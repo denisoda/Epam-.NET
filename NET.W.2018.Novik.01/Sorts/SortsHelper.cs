e6d31cad07ddbe5f4b15c1f2b3140fce79b7704e
Илья Новик <Indertt@gmail.com>
2018-03-13 22:37:00 +0300

    added tests and implemented sorts

---
+++ NET.W.2018.Novik.01/Sorts/SortsHelper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class SortsHelper
+    {
+
+        #region Merge Sort
+        /// <summary>
+        /// Merge Sort
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int[] MergerSort(int[] array)
+        {
+            return innerMergeSort(array, 0, array.Length - 1); // invoke inner mergersort
+        }
+        public static int[] innerMergeSort(int[] array, int start, int end)
+        {
+            if (start >= end) return new int[] { array[start] };
+
+            int middle = start + (end - start >> 1);
+
+            int[] left = innerMergeSort(array, start, middle);
+            int[] right = innerMergeSort(array, middle + 1, end);
+            int[] result = new int[left.Length + right.Length];
+
+            int indexLeft = 0;
+            int indexRight = 0;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (indexLeft >= left.Length) { result[i] = right[indexRight++]; continue; }
+                if (indexRight >= right.Length) { result[i] = left[indexLeft++]; continue; }
+
+                if (right[indexRight] >= left[indexLeft])
+                { result[i] = left[indexLeft++]; }
+                else
+                { result[i] = right[indexRight++]; }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Quick Sort
+        /// <summary>
+        /// Quick Sort
+        /// </summary>
+        /// <param name="array"></param>
+        public static void QuickSort(int[] array)
+        {
+            innerQuickSort(array, 0, array.Length - 1); // invoke inner quicksort
+        }
+
+        private static void innerQuickSort(int[] array, int left, int right)
+        {
+            int i = left, j = right;
+            int halfElement = array[left + (right - left >> 1)];
+
+            while (i <= j)
+            {
+                while (array[i] < halfElement)
+                {
+                    i++;
+                }
+
+                while (array[j] > halfElement)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                innerQuickSort(array, left, j);
+            }
+
+            if (i < right)
+            {
+                innerQuickSort(array, i, right);
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ NET.W.2018.Novik.01/Tests/UnitTestSorts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sorts;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTestSorts
+    {
+        /// <summary>
+        /// Test kind of sort from Sorts.SortsHelper
+        /// </summary>
+        
+        [TestMethod]
+        public void TestMegreSort()
+        {
+            //Arrange
+            int[] arrayGoal = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] arrayInput = new int[] { 1, 4, 3, 5, 6, 2 };
+
+            //Act
+            arrayInput = SortsHelper.MergerSort(arrayInput);
+
+            //Assert
+            Assert.IsTrue(arrayGoal.SequenceEqual(arrayInput));
+        }
+
+        [TestMethod]
+        public void TestQuickSort()
+        {
+            //Arrange
+            int[] arrayGoal = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] arrayInput = new int[] { 1, 4, 3, 5, 6, 2 };
+
+            //Act
+            SortsHelper.QuickSort(arrayInput);
+
+            //Assert
+            Assert.IsTrue(arrayGoal.SequenceEqual(arrayInput));
+        }
+    }
+}
